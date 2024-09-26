09d62a28ee7390849ccab39e33d12cabd6a6be56
abhilashkasula <abhilashkasula@users.noreply.github.com>
2024-09-26 13:58:57 +0000

    🚀 Deploy to pages 🚀

--- pokemon-submissions/pokemon-JAGADEESH2004PJ/src/pokemon.js
+++ pokemon-submissions/pokemon-JAGADEESH2004PJ/src/pokemon.js
@@ -1,63 +1,67 @@
+'use strict';
 const fetchConvertData = async (url) => {
   try {
     const response = await fetch(url);
     const pokemonData = await response.json();
     return pokemonData;
   } catch (error) {
-    console.log(`Error : ${error}`);
+    console.log(`Error: ${error}`);
   }
 };
 
 const fetchAllPokemons = async () => {
   const allPokemons = [];
+  const loadingElement = document.querySelector('#loading');
+  loadingElement.style.display = 'block';
   try {
-    const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset';
+    const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0';
     const data = await fetchConvertData(apiUrl);
-    console.log(data);
     for (const pokemon of data.results) {
-       allPokemons.push(pokemon);
+      allPokemons.push(pokemon);
     }
-    appendToDom(allPokemons);
-  } catch(error) {
-    console.log(`Error : ${error}`);
+    loadingElement.style.display = ''
+    for (let i = 0; i < allPokemons.length; i++) {
+      await appendToDom(allPokemons[i]);
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
   }
 };
 
-const appendToDom = async (allPokemons) => {
-  console.log(allPokemons);
-try {
-  for (const pokemon of allPokemons) {
+const appendToDom = async (pokemon) => {
+  try {
     const data = await fetchConvertData(pokemon.url);
     const card = document.createElement('div');
     card.className = 'pokemonCard';
     createAndAppend(data, card);
-  }
-} catch (error) {
-  console.log('Failed to load Pokémon data:', error);
-  } finally {
-  document.getElementById('loading').style.display = 'none';
+  } catch (error) {
+    console.log('Failed to load Pokémon data:', error);
   }
 };
 
-const createAndAppend = async (pokemonData, pokemonCard) => {
+const createAndAppend = (pokemonData, pokemonCard) => {
   const pokemonName = document.createElement('h2');
-  pokemonName.textContent = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
+  pokemonName.textContent = `${pokemonData.name.charAt(0).toUpperCase()}${pokemonData.name.slice(1)}`;
+
   const id = document.createElement('p');
   id.textContent = `ID: ${pokemonData.id}`;
+
   const img = document.createElement('img');
   img.src = pokemonData.sprites.front_default;
+
   img.alt = pokemonData.name;
   const types = document.createElement('p');
-  types.textContent = `Types: ${pokemonData.types[0].type.name}`;
 
+  types.textContent = `Types: ${pokemonData.types[0].type.name}`;
   pokemonCard.appendChild(pokemonName);
+
   pokemonCard.appendChild(id);
   pokemonCard.appendChild(img);
   pokemonCard.appendChild(types);
 
   document.getElementById('pokemonList').appendChild(pokemonCard);
-}
+};
 
-
-
-fetchAllPokemons();
+window.onload = () => {
+  fetchAllPokemons();
+};
--- pokemon-submissions/pokemon-Venkat14424/src/js/index.js
+++ pokemon-submissions/pokemon-Venkat14424/src/js/index.js
@@ -1,3 +1,37 @@
+'use strict'
+const appendPokemonName = (data) => {
+  const pokemonName = document.createElement('p');
+  pokemonName.innerHTML = "Name: " + data.name;
+  return pokemonName;
+}
+
+const appendPokemanId = (data) => {
+  const pokemonId = document.createElement('p');
+  pokemonId.innerHTML = "ID: " + data.id;
+  return pokemonId;
+}
+
+const appendPokemanImage = (data) => {
+  const pokemonImg = document.createElement('img');
+  pokemonImg.setAttribute('src', data.sprites.front_default);
+  return pokemonImg;
+}
+const appendPokemanType = (data) => {
+  const pokemonTypes = document.createElement('p');
+  pokemonTypes.innerHTML = "Types: " + data.types.map(type => type.type.name).join(', ');
+  return pokemonTypes;
+}
+const displayPokemon = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const pokemonData = document.createElement('section');
+  pokemonData.appendChild(appendPokemanImage(data));
+  pokemonData.appendChild(appendPokemanId(data));
+  pokemonData.appendChild(appendPokemonName(data));
+  pokemonData.appendChild(appendPokemanType(data));
+  document.getElementById('pokemonContainer').appendChild(pokemonData);
+}
+
 async function fetchPokemons() {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon-form/?offset=0&limit=200');
   const data = await response.json();
@@ -7,23 +41,4 @@ async function fetchPokemons() {
   });
 }
 
-const displayPokemon = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  const pokemonContainer = document.getElementById('pokemonContainer');
-  const pokemonData = document.createElement('section');
-  const pokemonImg = document.createElement('img');
-  const pokemonId = document.createElement('p');
-  const pokemonName = document.createElement('p');
-  const pokemonTypes = document.createElement('p');
-  pokemonName.innerHTML = "Name: " + data.name;
-  pokemonImg.setAttribute('src', data.sprites.front_default);
-  pokemonId.innerHTML = "ID: " + data.id;
-  pokemonTypes.innerHTML = "Types: " + data.types.map(type => type.type.name).join(', ');
-  pokemonData.appendChild(pokemonImg);
-  pokemonData.appendChild(pokemonId);
-  pokemonData.appendChild(pokemonName);
-  pokemonData.appendChild(pokemonTypes);
-  pokemonContainer.appendChild(pokemonData);
-}
 window.onload = fetchPokemons;
